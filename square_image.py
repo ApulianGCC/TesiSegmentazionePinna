75b0ad2a7d4060d1f83f72566fc7c7ec8329e71c
anto_guer <guerra.antonio98@gmail.com>
2021-08-11 13:11:58 +0200

    aggiunte funzioni per rendere quadrate le immagini

---
+++ square_image.py
@@ -0,0 +1,49 @@
+import os
+import PIL.Image
+
+
+def expand2square(pil_img, background_color):
+    width, height = pil_img.size
+    if width == height:
+        return pil_img
+    elif width > height:
+        result = PIL.Image.new(pil_img.mode, (width, width), background_color)
+        result.paste(pil_img, (0, (width - height) // 2))
+        return result
+    else:
+        result = PIL.Image.new(pil_img.mode, (height, height), background_color)
+        result.paste(pil_img, ((height - width) // 2, 0))
+        return result
+
+
+def createSquareImages(path, save_path, background_color):
+    for filename in os.listdir(path):
+        img = PIL.Image.open(path + "\\" + filename)
+        if img is not None:
+            if img.mode in ("RGBA", "P"):
+                img = img.convert("RGB")
+            square_img = expand2square(img, background_color)
+            square_img.save(save_path + filename)
+    return
+
+
+if __name__ == '__main__':
+    print("Eseguito script come main")
+
+    dirname = os.path.dirname(__file__)
+    pathInput = os.path.join(dirname, 'dataset\\originale\\input\\')
+
+    pathSaveInput = os.path.join(dirname, 'dataset\\originaleSquared\\input\\')
+
+    if not os.path.exists(pathSaveInput):
+        os.makedirs(pathSaveInput)
+
+    createSquareImages(pathInput, pathSaveInput, (255, 0, 0))
+
+    pathOutput = os.path.join(dirname, 'dataset\\originale\\output\\')
+    pathSaveOutput = os.path.join(dirname, 'dataset\\originaleSquared\\output\\')
+
+    if not os.path.exists(pathSaveOutput):
+        os.makedirs(pathSaveOutput)
+
+    createSquareImages(pathInput, pathSaveOutput, (0, 0, 0))
